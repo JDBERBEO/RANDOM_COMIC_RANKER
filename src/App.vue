f1e7191b6597e3e0ed1dbb4d7e08d82def09fe79
Juan David Berbeo <jd.berbeo@gmail.com>
2023-09-24 07:31:40 -0500

    fix: reset files

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #fff4e3;
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
